cccb5d1fdb47ca18e01f4773655624ba995fab85
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2018-10-19 09:46:56 +0200

    Indentation/unused vars cleanup

--- src/nn_primitives.f90
+++ src/nn_primitives.f90
@@ -1,115 +1,100 @@
-! FILE: FIB3.F
-      SUBROUTINE FIB(A,N)
-!
-!     CALCULATE FIRST N FIBONACCI NUMBERS
-!
-      INTEGER N
-      REAL*8 A(N)
-      INTEGER :: n_hidden_layers, n_hidden_nodes, n_inputs, n_outputs
-      CHARACTER(len=4), dimension(3) ::      hidden_activation
-      REAL, DIMENSION(:,:), ALLOCATABLE ::   weights_input
-      REAL, DIMENSION(:), ALLOCATABLE ::     biases_input
-      REAL, DIMENSION(:,:,:), ALLOCATABLE :: weights_hidden
-      REAL, DIMENSION(:,:), ALLOCATABLE ::   biases_hidden
-      REAL, DIMENSION(:,:), ALLOCATABLE ::   weights_output
+! file: fib3.f
+module qlknn
+    implicit none
+contains
+    subroutine fib()
+        integer i, lay
+        integer :: n_hidden_layers, n_hidden_nodes, n_inputs, n_outputs
+        character(len=4), dimension(3) ::      hidden_activation
+        real, dimension(:,:), allocatable ::   weights_input
+        real, dimension(:), allocatable ::     biases_input
+        real, dimension(:,:,:), allocatable :: weights_hidden
+        real, dimension(:,:), allocatable ::   biases_hidden
+        real, dimension(:,:), allocatable ::   weights_output
 
-      REAL, DIMENSION(:), ALLOCATABLE ::   feature_prescale_bias
-      REAL, DIMENSION(:), ALLOCATABLE ::   feature_prescale_factor
-      REAL, DIMENSION(:), ALLOCATABLE ::   target_prescale_bias
-      REAL, DIMENSION(:), ALLOCATABLE ::   target_prescale_factor
+        real, dimension(:), allocatable ::   feature_prescale_bias
+        real, dimension(:), allocatable ::   feature_prescale_factor
+        real, dimension(:), allocatable ::   target_prescale_bias
+        real, dimension(:), allocatable ::   target_prescale_factor
+        real :: biases_output
 
-      REAL, DIMENSION(:,:), ALLOCATABLE ::   res
-      namelist /sizes/ n_hidden_layers, n_hidden_nodes, n_inputs, n_outputs
-      type NetworkType
-        CHARACTER(len=4), dimension(3) :: hidden_activation
-        REAL, DIMENSION(:,:), ALLOCATABLE :: weights_input
-        REAL, DIMENSION(:), ALLOCATABLE :: biases_input
-        REAL, DIMENSION(:,:,:), ALLOCATABLE :: weights_hidden
-        REAL, DIMENSION(:,:), ALLOCATABLE :: biases_hidden
-        REAL, DIMENSION(:,:), ALLOCATABLE :: weights_output
-        REAL :: biases_output
-      end type NetworkType
+        real, dimension(:,:), allocatable ::   res
+        namelist /sizes/ n_hidden_layers, n_hidden_nodes, n_inputs, n_outputs
+        type networktype
+            character(len=4), dimension(3) :: hidden_activation
+            real, dimension(:,:), allocatable :: weights_input
+            real, dimension(:), allocatable :: biases_input
+            real, dimension(:,:,:), allocatable :: weights_hidden
+            real, dimension(:,:), allocatable :: biases_hidden
+            real, dimension(:,:), allocatable :: weights_output
+            real :: biases_output
+        end type networktype
 
-      namelist /net/ biases_hidden, hidden_activation, weights_input, biases_input, &
-       weights_hidden, weights_output, biases_output, &
-   feature_prescale_bias, feature_prescale_factor, target_prescale_bias,&
-   target_prescale_factor
-      REAL, DIMENSION(3, 9) :: inp
-      inp(1,:) = (/ 1.,2.,5.,2.,0.66,0.4,0.45,1.,1e-3 /)
-      inp(2,:) = (/ 1.,13.,5.,2.,0.66,0.4,0.45,1.,1e-3 /)
-      inp(3,:) = (/ 0, 0, 0, 0, 0, 0, 0, 0, 0 /)
-      !  Ati  Ate   An         q      smag         x  Ti_Te
-      !1.0   2.000000  5.0  2.0  0.660156  0.399902  0.449951    1.0      0.001
-      !1.0  13.000000  5.0  2.0  0.660156  0.399902  0.449951    1.0      0.001
+        namelist /net/ biases_hidden, hidden_activation, weights_input, biases_input, &
+            weights_hidden, weights_output, biases_output, &
+            feature_prescale_bias, feature_prescale_factor, target_prescale_bias,&
+            target_prescale_factor
+        real, dimension(3, 9) :: inp
+        inp(1,:) = (/ 1.,2.,5.,2.,0.66,0.4,0.45,1.,1e-3 /)
+        inp(2,:) = (/ 1.,13.,5.,2.,0.66,0.4,0.45,1.,1e-3 /)
+        inp(3,:) = (/ 0, 0, 0, 0, 0, 0, 0, 0, 0 /)
+        !  ati  ate   an         q      smag         x  ti_te
+        !1.0   2.000000  5.0  2.0  0.660156  0.399902  0.449951    1.0      0.001
+        !1.0  13.000000  5.0  2.0  0.660156  0.399902  0.449951    1.0      0.001
 
+        open(10,file='efiitg_gb.nml')
+        read(10,nml=sizes)
+        write(*, *) 'n_hidden_layers', n_hidden_layers
+        write(*, *) 'n_hidden_nodes', n_hidden_nodes
+        write(*, *) 'n_inputs', n_inputs
+        write(*, *) 'n_outputs', n_outputs
+        allocate(weights_input(n_hidden_nodes, n_inputs))
+        allocate(biases_input(n_hidden_nodes))
+        allocate(biases_hidden(n_hidden_layers-1, n_hidden_nodes))
+        allocate(weights_hidden(n_hidden_layers-1, n_hidden_nodes, n_hidden_nodes))
+        allocate(weights_output(1, n_hidden_nodes))
+        allocate(feature_prescale_bias(n_inputs))
+        allocate(feature_prescale_factor(n_inputs))
+        allocate(target_prescale_bias(n_outputs))
+        allocate(target_prescale_factor(n_outputs))
+        read(10,nml=net)
+        close(10)
+        !write(*,nml=net)
+        allocate(res(n_inputs, n_hidden_nodes))
 
-!f2py intent(in) n
-!f2py intent(out) a
-!f2py depend(n) a
-
-      DO I=1,N
-         IF (I.EQ.1) THEN
-            A(I) = 0.0D0
-         ELSEIF (I.EQ.2) THEN
-            A(I) = 1.0D0
-         ELSE 
-            A(I) = A(I-1) + A(I-2)
-         ENDIF
-      ENDDO
-       open(10,file='efiITG_GB.nml')
-       read(10,nml=sizes)
-       write(*, *) 'n_hidden_layers', n_hidden_layers
-       write(*, *) 'n_hidden_nodes', n_hidden_nodes
-       write(*, *) 'n_inputs', n_inputs
-       write(*, *) 'n_outputs', n_outputs
-       ALLOCATE(weights_input(n_hidden_nodes, n_inputs))
-       ALLOCATE(biases_input(n_hidden_nodes))
-       ALLOCATE(biases_hidden(n_hidden_layers-1, n_hidden_nodes))
-       ALLOCATE(weights_hidden(n_hidden_layers-1, n_hidden_nodes, n_hidden_nodes))
-       ALLOCATE(weights_output(1, n_hidden_nodes))
-       ALLOCATE(feature_prescale_bias(n_inputs))
-       ALLOCATE(feature_prescale_factor(n_inputs))
-       ALLOCATE(target_prescale_bias(n_outputs))
-       ALLOCATE(target_prescale_factor(n_outputs))
-       read(10,nml=net)
-       close(10)
-       !write(*,nml=net)
-       ALLOCATE(res(n_inputs, n_hidden_nodes))
-
-       write(*,*) 'net inp'
-       write(*,*) inp(1,:)
-       do i = 1, 3
+        write(*,*) 'net inp'
+        write(*,*) inp(1,:)
+        do i = 1, 3
         inp(i,:) = feature_prescale_factor * inp(i,:) + &
-        feature_prescale_bias
-       end do
+            feature_prescale_bias
+        end do
 
-       res = MATMUL(inp, TRANSPOSE(weights_input))
-       do i = 1, 3
-           res(i, :) = res(i, :) + biases_input
-       end do
-       res = TANH(res)
+        res = matmul(inp, transpose(weights_input))
+        do i = 1, 3
+        res(i, :) = res(i, :) + biases_input
+        end do
+        res = tanh(res)
 
-       do lay = 1, n_hidden_layers - 1
-         res = MATMUL(res, TRANSPOSE(weights_hidden(lay, :, :)))
-         do i = 1, 3
-             res(i, :) = res(i, :) + biases_hidden(lay, :)
-         end do
-         res = TANH(res)
-       end do
+        do lay = 1, n_hidden_layers - 1
+        res = matmul(res, transpose(weights_hidden(lay, :, :)))
+        do i = 1, 3
+        res(i, :) = res(i, :) + biases_hidden(lay, :)
+        end do
+        res = tanh(res)
+        end do
 
-       res = MATMUL(res, TRANSPOSE(weights_output))
-       do i = 1, 3
-           res(i, :) = res(i, :) + biases_output
-       end do
+        res = matmul(res, transpose(weights_output))
+        do i = 1, 3
+            res(i, :) = res(i, :) + biases_output
+        end do
 
-       do i = 1, 3
-        res(i,:) = DOT_PRODUCT(1/target_prescale_factor, res(i,:) - &
-        target_prescale_bias)
-       end do
-       write(*,*) 'net out'
-       write(*,*) res
+        do i = 1, 3
+        res(i,:) = dot_product(1/target_prescale_factor, res(i,:) - &
+            target_prescale_bias)
+        end do
+        write(*,*) 'net out'
+        write(*,*) res
 
 
-       end
-      END
-! END FILE FIB3.F
+    end subroutine fib
+end module qlknn
