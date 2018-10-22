099c9aa70d71571f047cdcea25c5ff87d00e05cc
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2018-10-22 13:48:52 +0200

    Read input from namelist

--- src/qlknn_test.f90
+++ src/qlknn_test.f90
@@ -1,11 +1,12 @@
 program qlknn_test
     use qlknn_primitives
     implicit none
-    real, dimension(10,3) :: input
+    real, dimension(10,24)::input
+    namelist /test/ input
     print *, "Hello World!"
-    input(:,1) = (/ 1.,2.,5.,2.,0.66,0.4,0.45,1.,1e-3, 0.2 /)
-    input(:,2) = (/ 1.,13.,5.,2.,0.66,0.4,0.45,1.,1e-3, 0.8 /)
-    input(:,3) = (/ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 /)
+    open(10,file='test.nml')
+    read(10,nml=test)
+
     call evaluate_QLKNN_10D(input)
 
 end program qlknn_test
