d745e277466b7d85403c3fb6b3a5fe36fa782a3a
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2018-10-22 13:49:26 +0200

    Added debugging, leading clipping and leading multiplication

--- src/qlknn_primitives.f90
+++ src/qlknn_primitives.f90
@@ -43,16 +43,16 @@ module qlknn_primitives
     use net_gam_leq_gb
 
     implicit none
-    include "mkl_vml.f90"
+    include "mkl_vml.fi"
     include "mkl_blas.fi"
 contains
     subroutine evaluate_QLKNN_10D(input)
         real, dimension(:,:), intent(in) :: input
 
-        integer trial, n_trails, n_rho, ii, n_nets, n_rotdiv
+        integer trial, n_trails, n_rho, ii, jj, rho, n_nets, n_rotdiv, idx
         real :: start, finish
         type(networktype), dimension(38) :: nets, rotdiv_nets
-        real, dimension(:), allocatable :: res
+        real, dimension(:), allocatable :: res, x, y
         real, dimension(:,:), allocatable :: net_result, rotdiv_result
         real, dimension(:), allocatable :: gam_leq
         real, dimension(:,:), allocatable :: net_input
@@ -60,7 +60,10 @@ contains
         type (qlknn_options) :: opts
         logical, dimension(20) :: net_evaluate
         logical, dimension(19) :: rotdiv_evaluate
-        !  ati  ate   an         q      smag         x  ti_te
+        integer, dimension(8), parameter :: idx_ITG = (/2, 6, 8, 10, 12, 14, 16, 18/)
+        integer, dimension(8), parameter :: idx_TEM = (/5, 7, 9, 11, 13, 15, 17, 19/)
+        integer, parameter :: leading_ITG = 4, leading_TEM = 3, leading_ETG = 1
+        ! zeff ati  ate   an         q      smag         x  ti_te logNustar
         !1.0   2.000000  5.0  2.0  0.660156  0.399902  0.449951    1.0      0.001
         !1.0  13.000000  5.0  2.0  0.660156  0.399902  0.449951    1.0      0.001
         nets( 1) = efeetg_gb()
@@ -111,11 +114,15 @@ contains
         allocate(rotdiv_input(8, n_rho))
         allocate(rotdiv_result(n_rho, n_rotdiv))
 
-        net_input = input(1:9, :)
-        rotdiv_input = input(1:8, :)
+        allocate(res(n_rho)) !Debug
 
-        write(*,*) 'input'
-        write(*,*) input(:,1)
+        net_input = input(1:9, :)
+        rotdiv_input = input((/3, 7, 5, 6, 2, 4, 8, 10/), :)
+
+        write(*,*) 'input, n_rho=', n_rho
+        do rho = 1, n_rho
+            WRITE(*,'(*(F7.2 X))'), (input(ii, rho), ii=1,10)
+        end do
         write(*,*) 'net_input'
         write(*,*) net_input(:,1)
         write(*,*) 'rotdiv_input'
@@ -125,26 +132,51 @@ contains
         call print_qlknn_options(opts)
         call get_networks_to_evaluate(opts, net_evaluate, rotdiv_evaluate)
         net_result = 0.
-        !rotdiv_result = 0.
-        !gam_leq = 0.
+
         write(*,*) net_evaluate, rotdiv_evaluate
+        ! Impose input constants
+
+        ! Evaluate all neural networks
         do ii =1,n_nets
             if (net_evaluate(ii)) then
                 call evaluate_network_mkl(net_input, nets(ii), net_result(:, ii))
             end if
         end do
-        !do ii =1,n_rotdiv
-        !    if (net_evaluate(ii)) then
-        !        call evaluate_network_mkl(rotdiv_input, rotdiv_nets(ii), rotdiv_result(:, ii))
-        !    end if
-        !end do
-        WRITE(*,'(AX,*(F7.2 X))') 'net_result'       , (net_result(1, ii), ii=1,n_nets)
-        WRITE(*,'(AX,*(F7.2 X))') 'rotdiv_result'       , (rotdiv_result(1, ii), ii=1,n_rotdiv)
+        do ii =1,n_rotdiv
+            if (rotdiv_evaluate(ii)) then
+                call evaluate_network_mkl(rotdiv_input, rotdiv_nets(ii), rotdiv_result(:, ii))
+            end if
+        end do
 
-        n_trails = 1000
-        allocate(res(n_rho))
+        ! Clip leading fluxes to 0
+        WRITE(*,*) net_result(:, (/leading_ETG, leading_ITG, leading_TEM/)).lt.0
+        WHERE (net_result(:, (/leading_ETG, leading_ITG, leading_TEM/)).lt.0) &
+                net_result(:, (/leading_ETG, leading_ITG, leading_TEM/)) = 0
+
+        ! Clip stable modes to 0, based on leading flux
+        do ii = 1, size(idx_ITG,1)
+            idx = idx_ITG(ii)
+            CALL vdmul(n_rho, net_result(:, idx), net_result(:, leading_ITG), net_result(:, idx))
+        end do
+        do ii = 1, size(idx_TEM,1)
+            idx = idx_TEM(ii)
+            CALL vdmul(n_rho, net_result(:, idx), net_result(:, leading_TEM), net_result(:, idx))
+        end do
+
+
+        WRITE(*,'(A)') 'net_result'
+        do rho = 1, n_rho
+            WRITE(*,'(*(F7.2 X))'), (net_result(rho, ii), ii=1,n_nets)
+        end do
+
+        WRITE(*,'(A)') 'rotdiv_result'
+        do rho = 1, n_rho
+            WRITE(*,'(*(F7.2 X))'), (rotdiv_result(rho, ii), ii=1,n_rotdiv)
+        end do
+
+        n_trails = 1
         do trial = 1,n_trails
-            CALL evaluate_network_mkl(net_input, nets(4), res)
+            CALL evaluate_network_mkl(net_input, nets(1), res, 0)
         end do
         call cpu_time(finish)
         print '("Time = ",f6.3," milliseconds.")',1000*(finish-start)/n_trails
@@ -154,17 +186,25 @@ contains
 
     end subroutine evaluate_QLKNN_10D
 
-    subroutine evaluate_network_mkl(input, net, output_1d)
+    subroutine evaluate_network_mkl(input, net, output_1d, verbosityin)
         real, dimension(:,:), intent(in) :: input
         type(networktype), intent(in) :: net
+        integer, optional, intent(in) :: verbosityin
+        integer:: verbosity
         real, dimension(:,:), allocatable ::   output
         real, dimension(:), intent(out) :: output_1d
         integer num
 
-        integer rho, lay
+        integer rho, lay, ii
         integer :: n_hidden_layers, n_hidden_nodes, n_inputs, n_outputs, n_rho
         real, dimension(:,:), allocatable :: inp_resc
         real, dimension(:,:), allocatable :: B_hidden, B_output
+
+        if(present(verbosityin))then
+            verbosity=verbosityin
+        else
+            verbosity = 0
+        end if
 
         n_hidden_layers = size(net%weights_hidden, 3) + 1
         n_hidden_nodes = size(net%weights_hidden, 2)
@@ -177,18 +217,29 @@ contains
         if (.NOT. size(output_1d) == n_rho) then
             ERROR STOP 'Passed output_1d has wrong shape!'
         end if
-        !write(*, *) 'n_hidden_layers', n_hidden_layers
-        !write(*, *) 'n_hidden_nodes', n_hidden_nodes
-        !write(*, *) 'n_inputs', n_inputs
-        !write(*, *) 'n_outputs', n_outputs
-        !write(*, *) 'n_rho', n_rho
+        if (.NOT. n_inputs == size(input, 1)) then
+            ERROR STOP 'Passed input has wrong number of inputs!'
+        end if
         allocate(inp_resc(lbound(input,1):ubound(input,1), lbound(input,2):ubound(input,2)))
         allocate(B_hidden(n_hidden_nodes, n_rho))
         allocate(B_output(n_outputs, n_rho))
 
+        if (verbosity >= 2) then
+            write(*, *) 'n_hidden_layers', n_hidden_layers
+            write(*, *) 'n_hidden_nodes', n_hidden_nodes
+            write(*, *) 'n_inputs', n_inputs
+            write(*, *) 'n_outputs', n_outputs
+            write(*, *) 'n_rho', n_rho
+            write(*,*) 'input'
+            do rho = 1, n_rho
+                write(*,'(*(f7.2 x))') input(:, rho)
+            end do
+        end if
+        if (verbosity >= 2) write(*,*) 'inp_resc'
         do rho = 1, n_rho
             inp_resc(:,rho) = net%feature_prescale_factor * input(:,rho) + &
                 net%feature_prescale_bias
+            if (verbosity >= 2) write(*,'(*(f7.2 x))') inp_resc(:, rho)
         end do
 
         do rho = 1, n_rho
@@ -218,13 +269,18 @@ contains
             output, n_hidden_nodes, &
             1., B_output, n_outputs)
         output = B_output
+        if (verbosity >= 2) write(*,*) 'output_layer'
+        if (verbosity >= 2) write(*,'(*(f7.2 x))') output
 
         do rho = 1, n_rho
             output(:,rho) = dot_product(1/net%target_prescale_factor, output(:,rho) - &
                 net%target_prescale_bias)
         end do
+        if (verbosity >= 2) write(*,*) 'output_descaled'
+        if (verbosity >= 2) write(*,'(*(f7.2 x))') output
 
         output_1d(:) = output(1, :)
+        !WRITE(*,'(*(F7.2 X))'), (output(1, ii), ii=1,n_rho)
         deallocate(inp_resc)
         deallocate(B_hidden)
         deallocate(B_output)
